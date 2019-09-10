5b4d6174a162a3b3a990700474f2b73f5c6a5958
Yin Li <eelregit@gmail.com>
2019-09-10 16:50:34 +0900

    Add authors

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages
 setup(
     name='supressim',
     version='0.0',
-    description='Super Resolutuion nbody Simulations',
-    author='Yin Li',
+    description='Simulation enhancers and transformers',
+    author='Yin Li, Yu Feng, et al.',
     author_email='eelregit@gmail.com',
     packages=find_packages(),
     install_requires=[
         'torch',
         'numpy',
-        'bigfile', # only for training data preprocessing
-    ], 
+        'bigfile',  # only for training data preprocessing
+    ],
     scripts=['scripts/srgan.py']
 )
