3687a9f25cf10c2eb962459705fc0d28393f224a
Yin Li <eelregit@gmail.com>
2019-11-12 16:46:06 -0500

    Add evaluation mode

--- supressim/util/fullbox.py
+++ supressim/util/fullbox.py
@@ -5,7 +5,7 @@ from supressim.srsgan import models
 from supressim.srsgan.datasets import *
 from collections import OrderedDict
 
-generator = models.GeneratorResNet(n_residual_blocks=2)  
+generator = models.GeneratorResNet(n_residual_blocks=2)
 gpath = '/home/yueying/scratch1/dmo-srsgan/test3/saved_models/generator_14.pth'
 d1 = (torch.load(gpath,map_location=lambda storage, loc: storage))
 d2 = OrderedDict([(k[7:], v) if k.startswith('module.') else (k, v) for k, v in d1.items()]) # strip module wrapped from data parallel
@@ -13,6 +13,7 @@ generator.load_state_dict(d2)
 
 device = torch.device('cpu')
 generator = generator.to(device)
+generator.eval()
 
 lrfile = "/home/yueying/scratch1/dmo-srsgan/full/lr_set4_004.npy"
 
